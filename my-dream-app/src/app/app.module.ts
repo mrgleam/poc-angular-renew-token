aa2bfc8b5e0639de0e1d6d484f4b0bb486e0b28d
Santi <santi@odds.team>
2021-01-08 18:08:54 +0700

    feat: initial register, login and home component
    add alert service and auth guard

--- my-dream-app/src/app/app.module.ts
+++ my-dream-app/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertComponent } from './_directives';
+import { AuthGuard } from './_guards';
+import { AlertService } from './_services';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
